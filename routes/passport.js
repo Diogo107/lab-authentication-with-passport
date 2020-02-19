ddbffe4864d2e3d868c08f80a66824888cac831d
Diogo Santos <diogo.filipe.santos107@gmail.com>
2020-02-19 17:19:30 +0000

    Not done. Still can't do it.

--- routes/passport.js
+++ routes/passport.js
@@ -4,9 +4,10 @@ const { Router } = require('express');
 const passportRouter = Router();
 
 // Require user model
-
+const User = require('../models/user');
 // Add bcrypt to encrypt passwords
-
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
 // Add passport
 
 const ensureLogin = require('connect-ensure-login');
@@ -14,6 +15,14 @@ const ensureLogin = require('connect-ensure-login');
 passportRouter.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res, next) => {
   const user = req.user;
   res.render('passport/private', {
+    user
+  });
+});
+
+//Is this?
+passportRouter.get('/sign-up', ensureLogin.ensureLoggedIn(), (req, res, next) => {
+  const user = req.user;
+  res.render('passport/sign-up', {
     user
   });
 });
